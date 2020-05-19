30d8bab18d848b834d21f2fd18ae71de5384ab1b
Alexandre Mertz <alexandre.mertz@energisme.com>
2020-05-19 16:11:38 +0200

    first commit

---
+++ src/TableMultiplication.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class TableMultiplication {
+    public static void main(String[] args) {
+        int x = 3;
+        System.out.println(String.format("Carré de multiplication par %s", x));
+        printTableMultiplication(carreMultiplication(x));
+    }
+
+    // Rédigez le corps de la méthode carreMultiplication pour qu'elle renvoie un tableau à deux dimensions d'entiers(int) contenant le carré de multiplication de coefficient x.
+    // Exemple: carré de multiplication pour x = 4:
+    // 1, 2, 3, 4
+    // 2, 4, 6, 8
+    // 3, 6, 9, 12
+    // 4, 8, 12, 16
+    public static int[][] carreMultiplication(int x) {
+        int[][] res = new int[x + 1][x + 1];
+
+        // EXERCISE
+        // END EXERCISE
+
+        return res;
+    }
+
+    public static void printTableMultiplication(int[][] res) {
+        for (int[] row : res) System.out.println(Arrays.toString(row));
+    }
+}
